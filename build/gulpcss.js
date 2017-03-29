0966d269dcd02075c181e87157d022b16c3edccb
yc <kixi@163.com>
2017-03-29 18:52:16 +0800

    模块化webpack配置，css生产完成，js生产未完成

--- build/gulpcss.js
+++ build/gulpcss.js
@@ -1,14 +1,19 @@
 const gulp = require('gulp')
       , $ = require('gulp-load-plugins')()
 
-const G = {
+let G = {
   entry: '',
-  env: {}
+  env: {},
+  production: false
 }
 
 export function makeCss(entry, env){
   G.entry = entry
   G.env = env
+  G.production = false
+  if (process.env.NODE_ENV == 'production') {
+    G.production = true
+  }
 
   for (let file in entry) {
     let srcFiles = entry[file]
@@ -22,7 +27,15 @@ function generateCss(src, target, env){
   gulp.src(src, { base: env.src }) 
   .pipe($.stylus())
   .pipe($.concatCss(target))
-  .pipe(gulp.dest(env.dist))
+  .pipe(
+    $.autoprefixer({
+      browsers: ['last 2 versions', 'not ie <= 8', 'Firefox > 20', 'iOS > 6', 'Android > 4'],
+      cascade: true
+    })
+  )
+  .pipe( $.if( G.production,  $.cleanCss({compatibility: 'ie8'})) )
+  .pipe( $.if( G.production,  $.md5({size: 10, separator: '__'}) ) )
+  .pipe( gulp.dest(env.dist) )
 }
 
 gulp.task('dev.css', function(){
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,3 +1,16 @@
+var argv = process.argv.slice(2)
+var margv = require('minimist')(argv);
+if (argv.length) {
+  if (margv.d) {
+    process.env.NODE_ENV = 'development'
+  }
+  if (margv.p) {
+    process.env.NODE_ENV = 'production'
+  }
+} else {
+  process.env.NODE_ENV = 'development'
+}
+
 import _ from 'lodash'
 import * as gulpcss from './build/gulpcss'
 import * as gulphtml from './build/gulphtml'
@@ -8,111 +21,20 @@ const fs = require('fs')
       , webpack = require('webpack')
       , glob = require('glob')
       , gulp = require('gulp')
-      , $ = require('gulp-load-plugins')()
-      , ExtractTextPlugin = require('extract-text-webpack-plugin')
-      , HtmlWebpackPlugin = require('html-webpack-plugin');
+      , $ = require('gulp-load-plugins')();
+      
 
 const configs = require('./configs/default')()
       , version = configs.version
-      , getEntry = require('./build/entry')
-      , alias = require('./build/webpack.alias')
+      , getEntry = require('./build/entry');
 
-const env = (process.env.NODE_ENV || 'development').toLowerCase();  // 'development' production
+const env = (process.env.NODE_ENV).toLowerCase();  // 'development' production
 
 const DIST = path.join(__dirname, './dist/out', version, (env=='development' ? 'dev' : '') )
       , CSSSRC = './public/css'
       , JSSRC = './public/js'
       , HTMLSRC = './public/html'
       , SRC3DS = './public/3ds'
-
-const adapter = {
-  html: function(data){
-    let htmls = []
-    for (var key in data) {
-      htmls.push(new HtmlWebpackPlugin({
-        filename: key+'.html',
-        template: data[key],
-        inject: false
-      }))
-    }
-    return htmls
-  }
-}
-
-let webpackConfig = {
-  entry: {},
-
-  output: {
-    path: DIST,
-    filename: "[name].js",
-    publicPath: '/dist/out/js/',
-    chunkFilename: '[name]_[id].js',
-    libraryTarget:'var'
-  },
-
-  externals: {
-    "react" : "React"
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use:[{
-          loader: "babel-loader",
-          options: { 
-            presets:["react", "es2015", "stage-0", "stage-1", "stage-3"],
-            plugins: [
-              "transform-runtime",
-              "add-module-exports",
-              "transform-decorators-legacy",
-              "transform-react-display-name"
-            ]
-          }
-        }]
-      },
-      {
-        test: /(\.ejs|\.html)$/,
-        use:[{
-          loader: "ejs-loader",
-          options: { variable: 'data'}
-        }]
-      },
-      { 
-        test: /\.styl$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          publicPath: '/css/',
-          use: ['css-loader?modules', 'stylus-loader']
-        })
-      }
-    ]
-  },
-  resolve:{
-    alias: alias,
-    extensions:[
-      ".js", 
-      ".jsx", 
-      ".json", 
-      '.html', 
-      '.ejs', 
-      '.pug', 
-      ".css", 
-      '.styl', 
-      '.less', 
-      '.hbs'
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename:  (getPath) => {
-        return getPath('css/[name]_dy.css').replace('css/js', 'css');
-      },
-      allChunks: true
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
-}
 
 // css
 const _cssEntry = getEntry(CSSSRC, {type: 'css'})
@@ -140,10 +62,15 @@ gulphtml.makeHtml(_htmlEntry, {
 
 // js
 const _jsEntry = getEntry(JSSRC, {type: 'js'})
+let webpackConfig = require('./build/webpack.config')(DIST)
 webpackConfig.entry = _jsEntry
 
 module.exports = webpackConfig
 
+
+
+
+// new webpack.IgnorePlugin(/vertx/), // https://github.com/webpack/webpack/issues/353
 
 // 生产环境
 // plugins: [
