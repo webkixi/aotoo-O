0f40397f66b00c3e60c70336d5c59b5273de0ab8
yc <kixi@163.com>
2017-06-08 01:40:16 +0800

    libs 代码整理

--- libs/index.js
+++ libs/index.js
@@ -1,6 +1,26 @@
 import base from './common/base'
 import time from './common/time'
-export {
-  ...base,
-  ...time
+
+const isClent = typeof window != 'undefined'
+
+function libs(){
+  if (isClent) {
+    return {
+      ...base,
+      ...time
+    }
+  } else {
+    const app = require('./common/app')
+    const docs = require('./common/docs')
+    const url = require('./common/url')
+    return {
+      ...base,
+      ...time,
+      ...app,
+      ...docs,
+      ...url
+    }
+  }
 }
+
+export default libs()
