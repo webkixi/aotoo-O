752764d10f9e023bc1ef5f2192a3fedc1284e411
yc <kixi@163.com>
2017-06-12 18:06:03 +0800

    Temp用于调试

---
+++ common/js/temp/aotoo/suba/index.js
@@ -0,0 +1,28 @@
+const React = (typeof React != 'undefined' ? React : require('react'))
+const transTree = require('./lib/tree')
+
+function $item(props, isreact){
+  const Item = require('./itemview/foxdiv')
+  if (!props) return Item
+  return <Item {...props} />
+}
+
+function $list(props, isreact){
+  const List = require('./listview')
+  if (!props) return List
+  return <List {...props} />
+}
+
+function $tree(props){
+  if ( Array.isArray(props.data) ) {
+    const treeData = transTree(props.data)
+    return $list({data: treeData})
+  }
+}
+
+module.exports = {
+  item: $item,
+  list: $list,
+  tree: $tree,
+  transTree: transTree
+}
