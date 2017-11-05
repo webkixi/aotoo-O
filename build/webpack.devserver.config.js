ed7f6795ac13633d62321a280b07cab89f3d26cf
yc <kixi@163.com>
2017-11-06 00:05:19 +0800

    编译，优化编译环境，配置化端口获取

--- build/webpack.devserver.config.js
+++ build/webpack.devserver.config.js
@@ -3,46 +3,46 @@ const path = require('path')
 const ejs = require('ejs')
 const appConfigs = require('../configs/index')()
 
-function queryParams(uri){
+function queryParams(uri) {
   let [cat, title, id, ...other] = uri.substring(1).split('/')
-  return {cat, title, id, other}
+  return { cat, title, id, other }
 }
 
-function valideExt(filename){
+function valideExt(filename) {
   const exts = ['.html']
   let accessExt = false
-  const ext = path.extname( filename)
-  if (exts.indexOf(ext)>-1) {
+  const ext = path.extname(filename)
+  if (exts.indexOf(ext) > -1) {
     accessExt = true
   }
   if (!ext) accessExt = true
   return accessExt
 }
 // server
-function wpServer(configs, opts){
+function wpServer(configs, opts) {
   const staticsPath = opts.env == 'development' ? appConfigs.static.dev : appConfigs.static
   return {
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
     },
-    historyApiFallback:{
+    historyApiFallback: {
       rewrites: [
         // { from: /^\/$/, to: '/html/index.html' },
         // { from: /^\/hello$/, to: '/html/hello.html' },
       ]
     },
     clientLogLevel: "info",
-    contentBase: configs.output.path+'/html/',
+    contentBase: configs.output.path + '/html/',
     publicPath: '/',
     staticOptions: {
       redirect: false
     },
-    setup: function(app){
+    setup: function (app) {
       app.engine('html', ejs.renderFile);
       app.set("view engine", "html");
-      app.set('views', configs.output.path+'/html/')
-      app.get(/\/css\/(.*)\.css$/, function(req, res){
+      app.set('views', configs.output.path + '/html/')
+      app.get(/\/css\/(.*)\.css$/, function (req, res) {
         const staticPath = path.join(configs.output.path, req._parsedUrl._raw)
         if (fs.existsSync(staticPath)) {
           res.sendFile(staticPath);
@@ -51,7 +51,7 @@ function wpServer(configs, opts){
         }
       })
 
-      app.get(/\/js\/(.*)\.(js|json)$/, function(req, res){
+      app.get(/\/js\/(.*)\.(js|json)$/, function (req, res) {
         const staticPath = path.join(configs.output.path, req._parsedUrl._raw)
         if (fs.existsSync(staticPath)) {
           res.sendFile(staticPath);
@@ -59,8 +59,8 @@ function wpServer(configs, opts){
           res.status(404).send('Sorry! file is not exist.');
         }
       })
-      
-      app.get(/\/img\/(.*)\.(ico|jpg|jpeg|png|gif)$/, function(req, res){
+
+      app.get(/\/img\/(.*)\.(ico|jpg|jpeg|png|gif)$/, function (req, res) {
         const staticPath = path.join(staticsPath.root, req._parsedUrl._raw)
         if (fs.existsSync(staticPath)) {
           res.sendFile(staticPath);
@@ -69,7 +69,7 @@ function wpServer(configs, opts){
         }
       })
 
-      app.get(/\/images\/(.*)\.(ico|jpg|jpeg|png|gif)$/, function(req, res){
+      app.get(/\/images\/(.*)\.(ico|jpg|jpeg|png|gif)$/, function (req, res) {
         const staticPath = path.join(staticsPath.root, req._parsedUrl._raw)
         if (fs.existsSync(staticPath)) {
           res.sendFile(staticPath);
@@ -78,7 +78,7 @@ function wpServer(configs, opts){
         }
       })
 
-      app.get('/mapper', function(req, res){
+      app.get('/mapper', function (req, res) {
         const staticPath = path.join(configs.output.path, 'mapfile.json')
         if (fs.existsSync(staticPath)) {
           res.sendFile(staticPath);
@@ -87,17 +87,17 @@ function wpServer(configs, opts){
         }
       })
 
-      app.get(/.*/, function(req, res) {
+      app.get(/.*/, function (req, res) {
         let url = queryParams(req._parsedUrl._raw)
         let router = appConfigs.root
         if (url.cat) {
           if (valideExt(url.cat)) router = url.cat
         }
-        if (url.title){
-          if (valideExt(url.title)) router += '/'+url.title
+        if (url.title) {
+          if (valideExt(url.title)) router += '/' + url.title
         }
-        if (url.id){
-          if (valideExt(url.id)) router += '/'+url.id
+        if (url.id) {
+          if (valideExt(url.id)) router += '/' + url.id
         }
 
         const staticfile = router.replace(/\//g, '-')
@@ -105,7 +105,7 @@ function wpServer(configs, opts){
         const Pagejs = `<script type="text/javascript" src="/js/${staticfile}.js" ></script>`
         const Commoncss = `<link rel="stylesheet" href="/css/common.css" />`
         const Commonjs = `<script type="text/javascript" src="/js/common.js" ></script>`
-        res.render(router, {pagejs: Pagejs, pagecss: Pagecss, commoncss: Commoncss, commonjs: Commonjs})
+        res.render(router, { pagejs: Pagejs, pagecss: Pagecss, commoncss: Commoncss, commonjs: Commonjs })
       });
     },
     hot: true,
@@ -121,7 +121,9 @@ function wpServer(configs, opts){
 }
 
 // proxy
-function wpProxy(configs){
+function wpProxy(configs) {
+  const myPort = appConfigs.port
+  const myTarget = 'http://localhost:' + myPort
   return {
     headers: {
       'Access-Control-Allow-Origin': '*',
@@ -137,10 +139,10 @@ function wpProxy(configs){
       poll: 1000
     },
     host: '0.0.0.0',
-    port: 8070,
+    port: myPort,
     proxy: {
       '*': {
-        target: 'http://localhost:8070',
+        target: myTarget,
         secure: false,
         changeOrigin: true
       }
@@ -149,7 +151,7 @@ function wpProxy(configs){
   }
 }
 
-module.exports = function(cfgs, opts){
+module.exports = function (cfgs, opts) {
   if (opts && opts.serviceType) {
     const stype = opts.serviceType
     if (stype.f) {
