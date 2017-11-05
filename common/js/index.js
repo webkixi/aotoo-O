ea78571c4de0218a0212e79d72c385b2478b5a52
yc <kixi@163.com>
2017-11-06 00:06:01 +0800

    全局，优化前端全局环境，优化jQuery.once方法

--- common/js/index.js
+++ common/js/index.js
@@ -12,9 +12,23 @@ const context  = (()=>isClient ? window : global)() || {}
 const libs = require('./libs')
 context.ajax = libs.ajax
 context.inject = libs.inject
-$.fn.once = function(type, tgt, fn){
+
+$.fn.once = function (type, tgt, fn) {
   this.off(type, fn)
   this.on(type, tgt, fn)
+  return this
 }
+
+// Aotoo.CombineClass.prototype.inject = function(){
+//   if (this.isClient) {
+//     if (this.config.theme && this.config.autoinject) {
+//       libs.inject.css(this.config.theme)  //注入样式
+//     }
+//     if (typeof src == 'function') {
+//       src(libs.inject)
+//     }
+//   }
+//   return this
+// }
 
 module.exports = aotoo_enveriment
