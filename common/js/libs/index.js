1e065b02b6469c0cafb504f39fbadb8d913fdf94
yc <kixi@163.com>
2017-11-22 14:26:27 +0800

    common 优化inject初始化注入方法

--- common/js/libs/index.js
+++ common/js/libs/index.js
@@ -1,11 +1,11 @@
 require('./times')
 const ajax = require('./ajax')
-const mapper = ajax.sync.get('/mapper')
-
-Aotoo.inject.mapper = mapper.responseJSON
-const injectInstance = Aotoo.inject.init()
+ajax.sync.get('/mapper').then(function(data) {
+  Aotoo.inject.mapper = data
+  Aotoo.inject.init()
+})
 
 module.exports = {
   ajax: ajax,
-  inject: injectInstance
+  inject: Aotoo.inject
 }
