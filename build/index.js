6fc2bcbca3279076270cf682937117a3fc4a1319
yc <kixi@163.com>
2018-04-03 16:17:28 +0800

    编译 --- 编译优化，编译时try catch，检测编译时错误

--- build/index.js
+++ build/index.js
@@ -20,146 +20,149 @@ const reDemo = /(.*\/test)/
       , del  = require('del')
 
 function buildStart(nm, opts){
+  try {
+    const configs  = require('../configs')(envConfig)
+        , version  = configs.version
+        , getEntry = require('./entry')
+        , mapFile  = require('./mapfile')
 
-  const configs  = require('../configs')(envConfig)
-      , version  = configs.version
-      , getEntry = require('./entry')
-      , mapFile  = require('./mapfile')
+    const DIST    = path.join( __dirname, '../dist/out', version, (env=='development' ? 'dev' : '') )
+        , DLLDIST = path.join( __dirname, '../dist/out/', 'dll' )
+        , CSSSRC  = path.join( __dirname, '../public/css')
+        , JSSRC   = path.join( __dirname, '../public/js')
+        , HTMLSRC = path.join( __dirname, '../public/html')
+        , IMAGESSRC = path.join( __dirname, '../public/images')
+        , SRC3DS  = path.join( __dirname, '../public/3ds')
 
-  const DIST    = path.join( __dirname, '../dist/out', version, (env=='development' ? 'dev' : '') )
-      , DLLDIST = path.join( __dirname, '../dist/out/', 'dll' )
-      , CSSSRC  = path.join( __dirname, '../public/css')
-      , JSSRC   = path.join( __dirname, '../public/js')
-      , HTMLSRC = path.join( __dirname, '../public/html')
-      , IMAGESSRC = path.join( __dirname, '../public/images')
-      , SRC3DS  = path.join( __dirname, '../public/3ds')
+    del.sync([ DIST ], { force: true })
 
-  del.sync([ DIST ], { force: true })
+    // 第三方库
+    const treedsEnv = { dist: DIST }
+    gulp3ds.js(SRC3DS, treedsEnv)
+    gulp3ds.css(SRC3DS, treedsEnv)
 
-  // 第三方库
-  const treedsEnv = { dist: DIST }
-  gulp3ds.js(SRC3DS, treedsEnv)
-  gulp3ds.css(SRC3DS, treedsEnv)
-
-  // css
-  let _cssEntry = getEntry(CSSSRC, {type: 'css'})
-  _cssEntry = _.merge(_cssEntry, { 'css/common': path.join(__dirname, '../common/css/index.styl') })  // 加入 common.css
-  const _htmlEntry = getEntry(HTMLSRC, {type: 'html'})
-  const _jsEntry = getEntry(JSSRC, {type: 'js'})
-  
-  // 生产环境剔除demo
-  if (env == 'production') {
-    Object.keys(_cssEntry).forEach(key => {
-      if (reDemo.test(key)) delete _cssEntry[key]
-    })
-
-    Object.keys(_htmlEntry).forEach(key => {
-      if (reDemo.test(key)) delete _htmlEntry[key]
-    })
-
-    Object.keys(_jsEntry).forEach( key => {
-      if (reDemo.test(key)) delete _jsEntry[key]
-    })
-  }
-
-  // 生成 css
-  gulpcss.makeCss(_cssEntry, {
-    src: CSSSRC,
-    dist: DIST
-  })
-
-  // 生成 html
-  gulphtml.makeHtml(_htmlEntry, {
-    src: HTMLSRC,
-    dist: DIST+'/html'
-  })
-
-  // 生成 js config
-  let webpackConfig = require('./webpack.config').webpackConfig(_jsEntry, {
-    dist: DIST,
-    dlldist: DLLDIST
-  })
-
-  const dllConfig = require('./webpack.config').dllConfig({
-    dist: DLLDIST
-  })
-
-  const mapoptions = {
-    dist: DIST,
-    imagessrc: IMAGESSRC,
-    configs: configs,
-    entry: {
-      js: _jsEntry
-    }
-  }
-
-
-  // js build or start webpack dev server
-  function start(nm, opts){
-    // webpackConfig.plugins.push(
-    //   new webpack.DllReferencePlugin({
-    //     manifest: require(path.join(DLLDIST, 'precommon-manifest.json'))
-    //   })
-    // )
+    // css
+    let _cssEntry = getEntry(CSSSRC, {type: 'css'})
+    _cssEntry = _.merge(_cssEntry, { 'css/common': path.join(__dirname, '../common/css/index.styl') })  // 加入 common.css
+    const _htmlEntry = getEntry(HTMLSRC, {type: 'html'})
+    const _jsEntry = getEntry(JSSRC, {type: 'js'})
     
-    var compiler = webpack(webpackConfig)
+    // 生产环境剔除demo
     if (env == 'production') {
-      compiler.run( (err, stats) => {
-        if (err) throw new gutil.PluginError('[webpack]', err)
-        mapFile(mapoptions, ()=>{
-          process.exit()
-        })
+      Object.keys(_cssEntry).forEach(key => {
+        if (reDemo.test(key)) delete _cssEntry[key]
       })
-    } 
-    else {
-      const Delay = {
-        dev: [1200, 1300],
-        fed: [1000, 3000]
+
+      Object.keys(_htmlEntry).forEach(key => {
+        if (reDemo.test(key)) delete _htmlEntry[key]
+      })
+
+      Object.keys(_jsEntry).forEach( key => {
+        if (reDemo.test(key)) delete _jsEntry[key]
+      })
+    }
+
+    // 生成 css
+    gulpcss.makeCss(_cssEntry, {
+      src: CSSSRC,
+      dist: DIST
+    })
+
+    // 生成 html
+    gulphtml.makeHtml(_htmlEntry, {
+      src: HTMLSRC,
+      dist: DIST+'/html'
+    })
+
+    // 生成 js config
+    let webpackConfig = require('./webpack.config').webpackConfig(_jsEntry, {
+      dist: DIST,
+      dlldist: DLLDIST
+    })
+
+    const dllConfig = require('./webpack.config').dllConfig({
+      dist: DLLDIST
+    })
+
+    const mapoptions = {
+      dist: DIST,
+      imagessrc: IMAGESSRC,
+      configs: configs,
+      entry: {
+        js: _jsEntry
       }
+    }
+
+
+    // js build or start webpack dev server
+    function start(nm, opts){
+      // webpackConfig.plugins.push(
+      //   new webpack.DllReferencePlugin({
+      //     manifest: require(path.join(DLLDIST, 'precommon-manifest.json'))
+      //   })
+      // )
       
-      let delay = Delay.fed
-      if (nm && nm.emit) {
-        delay = Delay.dev
-      }
-
-      // 生成mapfile.json
-      let initRestartNodeServer = true
-      compiler.plugin('done', function (stats) {
-        mapFile(mapoptions, { delay: delay }, function (params) {
-          if (initRestartNodeServer) {
-            initRestartNodeServer = false
-            if (nm && nm.emit) nm.emit('restart')
-          }
+      var compiler = webpack(webpackConfig)
+      if (env == 'production') {
+        compiler.run( (err, stats) => {
+          if (err) throw new gutil.PluginError('[webpack]', err)
+          mapFile(mapoptions, ()=>{
+            process.exit()
+          })
         })
-      })
+      } 
+      else {
+        const Delay = {
+          dev: [1200, 1300],
+          fed: [1000, 3000]
+        }
+        
+        let delay = Delay.fed
+        if (nm && nm.emit) {
+          delay = Delay.dev
+        }
 
-      // opts.serviceType 用于启动webpack-dev-server的服务模式或者代理模式
-      // opts.statics 静态文件存放的绝对路径
-      opts.env = env
-      const devServer = new WebpackDevServer( compiler, require('./webpack.devserver.config')(webpackConfig, opts))
-      devServer.listen(configs.proxyPort, 'localhost', function (err, result) {
-        if (err) console.log(err);
-        console.log('Listening at http://localhost:'+configs.proxyPort);
-      })
+        // 生成mapfile.json
+        let initRestartNodeServer = true
+        compiler.plugin('done', function (stats) {
+          mapFile(mapoptions, { delay: delay }, function (params) {
+            if (initRestartNodeServer) {
+              initRestartNodeServer = false
+              if (nm && nm.emit) nm.emit('restart')
+            }
+          })
+        })
+
+        // opts.serviceType 用于启动webpack-dev-server的服务模式或者代理模式
+        // opts.statics 静态文件存放的绝对路径
+        opts.env = env
+        const devServer = new WebpackDevServer( compiler, require('./webpack.devserver.config')(webpackConfig, opts))
+        devServer.listen(configs.proxyPort, 'localhost', function (err, result) {
+          if (err) console.log(err);
+          console.log('Listening at http://localhost:'+configs.proxyPort);
+        })
+      }
     }
-  }
 
 
-  function prepareDll(nm, opts={serviceType:{}}) {
-    if (fs.existsSync(path.join(DLLDIST, 'precommon.js')) && !opts.serviceType.clean) {
-      start(nm, opts)
-    } else {
-      del.sync([ DLLDIST ], { force: true })
-      // 先编译 precommon.js
-      webpack(dllConfig).run( (err, stats) => {
-        if (err) throw new gutil.PluginError('[webpack]', err)
+    function prepareDll(nm, opts={serviceType:{}}) {
+      if (fs.existsSync(path.join(DLLDIST, 'precommon.js')) && !opts.serviceType.clean) {
         start(nm, opts)
-      })
+      } else {
+        del.sync([ DLLDIST ], { force: true })
+        // 先编译 precommon.js
+        webpack(dllConfig).run( (err, stats) => {
+          if (err) throw new gutil.PluginError('[webpack]', err)
+          start(nm, opts)
+        })
+      }
     }
+
+
+    // ready go go go
+    prepareDll(nm, opts)
+  } catch (error) {
+    console.log(error);
   }
-
-
-  // ready go go go
-  prepareDll(nm, opts)
 }
 module.exports = buildStart
